fded5aedb8306c1e017f7063f3a8968301984b8d
Hector lenin Cantarero moncada <142702365+HectorLenin@users.noreply.github.com>
2026-08-04 10:23:56 -0600

    Delete start.bat (Windows).bat
    delete

--- start.bat (Windows).bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo ========================================
-echo   REENVIO NETFLIX - INICIANDO
-echo ========================================
-cd backend
-npm install
-npm start
-pause
