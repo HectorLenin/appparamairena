c6f15621a407d3db3f95254b9703ae956fd672d5
Hector lenin Cantarero moncada <142702365+HectorLenin@users.noreply.github.com>
2026-08-04 09:28:37 -0600

    Add files via upload
    todos los archivos

---
+++ start.bat (Windows).bat
@@ -0,0 +1,8 @@
+@echo off
+echo ========================================
+echo   REENVIO NETFLIX - INICIANDO
+echo ========================================
+cd backend
+npm install
+npm start
+pause
